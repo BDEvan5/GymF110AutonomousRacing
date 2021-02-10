604ee5f52a07e60fc06c55707e07b51cf4673ee3
Benjamin <benjaminevans316@gmail.com>
2021-02-10 12:05:57 +0000

    My TunerCar working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import gym
 import time 
 import numpy as np 
 
-from TunerCar import TunerCar, PurePursuitPlanner
+from TunerCar import TunerCar
 import LibFunctions as lib
 
 def test_vehicle(conf, vehicle):
@@ -10,7 +10,6 @@ def test_vehicle(conf, vehicle):
     env = gym.make('f110_gym:f110-v0', map=conf.map_name, map_ext=conf.map_ext, num_agents=1)
     obs, step_reward, done, info = env.reset(np.array([[conf.sx, conf.sy, conf.stheta]]))
     env.render()
-
 
     laptime = 0.0
     start = time.time()
@@ -27,13 +26,11 @@ def test_vehicle(conf, vehicle):
 
 
 def run_tuner_car():
-    # work = {'mass': 3.463388126201571, 'lf': 0.15597534362552312, 'tlad': 0.82461887897713965, 'vgain': 0.90338203837889}
 
     config_file = "config_test"
     conf = lib.load_config_namespace(config_file)
 
     vehicle = TunerCar(conf)
-    vehicle = PurePursuitPlanner(conf)
 
     test_vehicle(conf, vehicle)
 
